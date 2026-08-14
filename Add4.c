ccd8ffb3c13f71c5fd9c8a54acf3c62b50fb654b
shreyap13485-boop <shreyap13485@gmail.com>
2026-08-14 07:03:11 +0000

    this code is add4

---
+++ Add4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c, d, sum;
+    printf("Enter 4 numbers:");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    sum=a+b+c+d;
+    printf("Addition=%d", sum);
+    return 0;
+}
+
+
