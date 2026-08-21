b8225bf771e21a86e693233e81d446bc691b32a2
shreyap13485-boop <shreyap13485@gmail.com>
2026-08-21 13:11:05 +0530

    Update Saving.cpp

--- Saving.cpp
+++ Saving.cpp
@@ -1,1 +1,101 @@
+#include<iostream>
+using namespace std;
+class SavingAccount{
+private:
+string accountHolderName;
+int accountNumber;
+double balance;
+double interestRate;
+public:
+SavingAccount(string name, int accNumber,double initialBalance, double rate){
+accountHolderName=name;
+accountNumber=accNumber;
+balance=initialBalance;
+interestRate=rate;
+}
+void deposit(double amount){
+  if(amount>0){
+balance+=amount;
+cout<<"Deposited:₹"<<amount<<endl;
+  }
+}
+void withdraw(double amount){
+  if(amount>0 && amount<=balance){
+balance=amount;
+cout<<"Withdrawn:₹"<<amount<<endl;
+  } else {
+cout<<"Insufficient balance!"<<endl;
+  }
+}
+void applyInterest() {
+  double interest=balance * interestRate/100;
+balance+=interest;
+cout<<"Interest Applied:₹"<<interest<<endl;
+}
+void display()
+cout<<"\n[Savings Account]"<<endl;
+cout<<"Account Holder:"<<accountHolderName<<endl;
+cout<<"Account Number:"<<accountNumber<<endl;
+cout<<"Balance:₹"<<balance<<endl;
+cout<<"Interest Rate:"<<interestRate<<"%"<<endl;
+}
+};
+class CheckingAccount{
+private:
+string accountHolderName;
+int accountNumber;
+double balance;
+double transcationFee;
+public:
+CheckingAccount(string name, int accNumber, double initialBalance, double fee) {
+accountHolderName=name;
+accountNumber=accNumber;
+balance=initialBalance;
+transcationFee=fee;
+}
+void depoisit(double amount){
+if(amount>0)[
+balance+=amount;
+cout<<"Deposited:₹"<<amount<<endl;
+}
+}
+void Withdraw(double amount){
+  double total+amount=transcationFee;
+if(total<=balance){
+balance=total;
+cout<<"Withdrawn:₹"<<amount<<"(₹"<<transcationFee<<"fee applied)"<<
+  endl;
+} else {
+cout<<"Insufficient balance for withdrawal+fee!"<<endl;
+}
+}
+void display(){
+  cout<<"\n[checking Account]"<<endl;
+cout<<"Account Holder:"<<accountHolderName<<endl;
+cout<<"Account Number:"<<accountNumber<<endl;
+cout<<"Balance:₹"<<balance<<endl;
+cout<<"Transcation Fee:₹"<<transactionFee<<endl;
+}
+};
+int main(){
+  SavingAccount savings("Alice", 1001, 5000.0, 3.0);
+CheckingAccount checking("Bob", 1002, 3000.0, 20.0);
+savings.display();
+savings.deposit(1000);
+savings.withdraw(2000);
+savings.applyInterest();
+savings.display();
+checking.display();
+checking.deposit(1500);
+checking.withdraw(1000);
+checking.display();
+return 0;
+}
+
+  
+  
+  
+
+
+
 
