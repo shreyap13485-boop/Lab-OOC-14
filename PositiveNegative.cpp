b7cc2de324c4a061091cf0e55eedd8cab93212ae
shreyap13485-boop <shreyap13485@gmail.com>
2026-09-04 07:39:58 +0000

    this code is positive or negative no.

---
+++ PositiveNegative.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout <<"Enter a number:";
+    cin >>n;
+    if(n>0)
+    cout <<"Number is Positive";
+else if(n<0)
+cout <<"Number is Negative";
+else
+cout <<"Number is Zero";
+return 0;
+}
